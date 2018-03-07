c76fe49ecaca4f11ff83609949e23e36f5eadd9c
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-03-07 21:33:12 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -60,8 +60,8 @@ end
 
 def can_be_found_updated_and_saved
   # Update the title "Awesome Flick" to "Even Awesomer Flick"
-  movie = Movie.create(title: "Awesome Flick")
-  # movie = Movie.find_by(title: "Awesome Flick")
+  Movie.create(title: "Awesome Flick")
+  movie = Movie.find_by(title: "Awesome Flick")
   movie.update(title: "Even Awesomer Flick")
   # movie.save
 end
